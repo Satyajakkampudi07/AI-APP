2ecf5a5894f866548cb987088a61f50e349f4539
SatyaJakkampudi07 <satishjakkampudi0@gmail.com>
2024-05-25 12:02:23 +0530

    added logout functionality

--- app/src/main/java/com/example/aiapp/MainActivity.kt
+++ app/src/main/java/com/example/aiapp/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.example.aiapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.aiapp.databinding.ActivityMainBinding
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var auth:FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+        binding.signOutButton.setOnClickListener {
+            if(auth.currentUser!=null)
+            {
+                auth.signOut()
+                startActivity(Intent(this,signInActivity::class.java))
+                finish()
+            }
+        }
 
         val eTPrompt= findViewById<EditText>(R.id.eTPrompt)
         val btnSubmit= findViewById<Button>(R.id.btnSubmit)
